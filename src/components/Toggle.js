c06ec5aafde9c66bd8a5a2eb8f705f56f1dffd5d
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2023-05-21 03:24:20 +0300

    Completed

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [status,setStatus] = useState("OFF");
+  const color = status === "ON"? "red" :"white";
+
+  return <button style ={{background: color}}onClick={() => status=== "ON"? setStatus("OFF"):setStatus("ON")}> {status}</button>;
 }
 
 export default Toggle;
